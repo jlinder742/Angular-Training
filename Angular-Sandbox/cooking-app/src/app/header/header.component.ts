76fd3a895ac00c797e11bda084fc5785353d0d1d
jlinder742 <jlinder742@gmail.com>
2023-05-18 10:26:46 -0500

    Added data binding
    Added data binding to the components

--- Angular-Sandbox/cooking-app/src/app/header/header.component.ts
+++ Angular-Sandbox/cooking-app/src/app/header/header.component.ts
@@ -1,10 +1,16 @@
 //For angular to recognize this as a component we must use the component decorator
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output} from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    //@output allows the "featureSelected" to be used by parent components
+    @Output() featureSelected = new EventEmitter<string>();
+    onSelect(feature: string)
+    {
+        this.featureSelected.emit(feature);
+    }
 
 }
